e0d81e1ddba37607aaa4a5e35b500d843f414645
Nico Prananta <nicnocquee@users.noreply.github.com>
2021-03-01 22:35:46 +0100

    chore: add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-import { Command, flags } from "@oclif/command";
+import { Command, flags } from '@oclif/command'
 
 class SymonAgent extends Command {
-  static description = "describe the command here";
+  static description = 'describe the command here'
 
   static flags = {
     // add --version flag to show CLI version
-    version: flags.version({ char: "v" }),
-    help: flags.help({ char: "h" }),
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: "n", description: "name to print" }),
+    name: flags.string({ char: 'n', description: 'name to print' }),
     // flag with no value (-f, --force)
-    force: flags.boolean({ char: "f" }),
-  };
+    force: flags.boolean({ char: 'f' }),
+  }
 
-  static args = [{ name: "file" }];
+  static args = [{ name: 'file' }]
 
   async run() {
-    const { args, flags } = this.parse(SymonAgent);
+    const { args, flags } = this.parse(SymonAgent)
 
-    const name = flags.name ?? "world";
+    const name = flags.name ?? 'world'
 
-    this.log(`hello ${name} from ./src/index.ts`);
+    this.log(`hello ${name} from ./src/index.ts`)
 
     if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+      this.log(`you input --force and --file: ${args.file}`)
     }
   }
 }
 
-export = SymonAgent;
+export = SymonAgent
